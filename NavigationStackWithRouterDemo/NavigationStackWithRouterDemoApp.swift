585d66239f052ad4efc9a0dca9ee7f40d0966ee0
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2025-01-27 16:41:02 +0200

    Initial Commit

---
+++ NavigationStackWithRouterDemo/NavigationStackWithRouterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackWithRouterDemoApp.swift
+//  NavigationStackWithRouterDemo
+//
+//  Created by ahmed hussien on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackWithRouterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
