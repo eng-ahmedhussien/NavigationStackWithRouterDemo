698824be22cc1631ab5c50bb8a3c0c8b50bffa9f
ahmed hussien <hussienhussien2016@gmail.com>
2025-02-23 10:04:39 +0200

    navigation with on navigation stack and one path
    navigation with on navigation stack and one path

--- NavigationStackWithRouterDemo/ContentView.swift
+++ NavigationStackWithRouterDemo/ContentView.swift
@@ -9,16 +9,135 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        RouterView {
+            TapBarView()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ViewA: View {
+    @EnvironmentObject var router: Router
+    @State private var showNavigationBar = true
+    var body: some View {
+        NavigationView{
+            VStack() {
+                Button("Go to View B") {
+                    router.navigateTo(.viewB(""))
+                }
+                Button("Go to View C") {
+                    router.navigateTo(.viewC)
+                }
+                
+                Button("Show ViewB (Modal)") {
+                    router.showView(.SheetView)
+                }
+                
+                Button("backTo Root") {
+                    router.popToRoot()
+                }
+                
+                NavigationLink("View Details") {
+                    DetailView()
+                }
+             
+                
+            }
+           
+            .navigationBarWithLogo()
+            .navigationTitle("View A")
+        }
+       
+    }
+}
+
+struct ViewB: View {
+    @EnvironmentObject var router: Router
+    let description: String
+    
+    var body: some View {
+        VStack() {
+            Text(description)
+            Button("Go to View A") {
+                router.navigateTo(.viewA)
+            }
+            Button("Go to View C") {
+                router.navigateTo(.viewC)
+            }
+            
+            Button("Show ViewB (Modal)") {
+                router.showView(.SheetView)
+            }
+            Button("backTo Root") {
+                router.popToRoot()
+            }
+        }
+        .navigationTitle("View B")
+    }
+}
+
+struct ViewC: View {
+    @EnvironmentObject var router: Router
+    
+    var body: some View {
+        VStack() {
+            Button("Go to View B") {
+                router.navigateTo(.viewB("Got here from C"))
+            }
+            Button("Go back") {
+                router.navigateBack()
+            }
+            Button("backTo Root") {
+                router.popToRoot()
+            }
+        }
+       
+        .navigationTitle("View C")
+    }
+}
+
+struct SheetView: View {
+    @EnvironmentObject var router: Router
+    
+    var body: some View {
+        VStack() {
+            Button("Go to View B") {
+                router.navigateTo(.viewB("Got here from C"))
+            }
+            Button("Go back") {
+                router.navigateBack()
+            }
+            
+            Button("Dismiss") {
+                router.dismissPresentedView() // Dismiss the full-screen modal
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+            
+            Button("backTo Root") {
+                router.popToRoot()
+            }
+        }
+        .navigationTitle("View C")
+    }
+}
+
+
+struct DetailView: View {
+    @State private var showNavigationBar = true
+
+    var body: some View {
+        Text("Detail View")
+            .toolbar(showNavigationBar ? .visible : .hidden,for: .tabBar)
+            .onTapGesture {
+                withAnimation {
+                    showNavigationBar.toggle()
+                }
+            }
+    }
 }
---
+++ NavigationStackWithRouterDemo/TabparView.swift
@@ -0,0 +1,155 @@
+//
+//  TabparView.swift
+//  NavigationStackWithRouterDemo
+//
+//  Created by ahmed hussien on 27/01/2025.
+//
+
+import SwiftUI
+
+struct TapBarView: View {
+    
+    @State var selectionTap: TabBarItem = .home
+    
+    init() {
+        let image = UIImage.gradientImageWithBounds(
+            bounds: CGRect( x: 0, y: 0, width: UIScreen.main.scale, height: 8),
+            colors: [
+                UIColor.clear.cgColor,
+                UIColor.black.withAlphaComponent(0.1).cgColor
+            ]
+        )
+        let appearance = UITabBarAppearance()
+       // appearance.configureWithTransparentBackground()
+      appearance.stackedLayoutAppearance.normal.badgeBackgroundColor = UIColor(.red)
+       appearance.backgroundColor =  UIColor(.white)
+        appearance.shadowImage = image
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        
+    }
+    
+    var body: some View {
+        
+        TabView(selection: $selectionTap) {
+
+            CustomTabItem(imageName: "house.fill",
+                          title:
+                            "HomeTapBar",
+                          tag: TabBarItem.home) {
+                ViewA()
+                   
+                    .onAppear {
+                        // UITabBar.showTabBar(animated: true)
+                    }
+                    .onDisappear {
+                        if selectionTap == .home {
+                            // UITabBar.hideTabBar()
+                        }
+                    }
+            }
+            
+            CustomTabItem(imageName: "server.rack", title:"servicesTapBar", tag: TabBarItem.services) {
+                ViewB( description: " ViewB()")
+                    .onAppear {
+                        // UITabBar.showTabBar(animated: true)
+                    }
+                    .onDisappear {
+                        if selectionTap == .services {
+                            // UITabBar.hideTabBar()
+                        }
+                    }
+            }
+            
+            CustomTabItem(imageName: "cart.fill",
+                          title: "cartTapBar",
+                          tag: TabBarItem.cart) {
+                ViewB( description: " ViewB()")
+                    .onAppear {
+                        // UITabBar.showTabBar(animated: true)
+                    }
+                    .onDisappear {
+                        if selectionTap == .cart {
+                            // UITabBar.hideTabBar()
+                        }
+                    }
+            }
+        }
+        .accentColor(.black)
+        
+        
+    }
+}
+
+//MARK: - CustomTabItem
+struct CustomTabItem<Content: View>: View {
+    var imageName: String
+    var title: String
+    var tag: TabBarItem
+    var content: Content
+    
+    init(imageName: String, title: String,tag: TabBarItem,@ViewBuilder content: () -> Content) {
+        self.imageName = imageName
+        self.title = title
+        self.tag = tag
+        self.content = content()
+    }
+    
+    var body: some View {
+        content
+            .tabItem {
+                VStack {
+                    Image(systemName: imageName)
+                    Text(title)
+                }
+            }
+            .tag(tag)
+            .navigationTitle(title)
+    }
+}
+
+extension UIImage {
+    static func gradientImageWithBounds(bounds: CGRect, colors: [CGColor]) -> UIImage {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
+
+enum TabBarItem: Int, Hashable {
+    case home = 0
+    case services = 1
+    case cart = 2
+    
+    var imageName: String{
+        switch self {
+        case .home:
+            return  "house.fill"
+        case .services:
+            return  "server.rack"
+        case .cart:
+            return  "cart.fill"
+        }
+    }
+    
+    var title: String{
+        switch self {
+        case .home:
+            return  "HomeTapBar"
+        case .services:
+            return  "servicesTapBar"
+        case .cart:
+            return  "cartTapBar"
+        }
+    }
+}
+
+#Preview {
+    TapBarView()
+}
